6f91976674d66228b3091119ee7fd9ed3a10bd16
Juan Rafael Garcia Blanco <juanrgar@marvell.com>
2022-05-06 11:01:33 +0200

    Web app skeleton

---
+++ run_web_app.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export FLASK_APP=web_app.py
+export FLASK_ENV=development
+flask run
---
+++ web_app.py
@@ -0,0 +1,17 @@
+import sqlite3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect('coles_cam.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    schools = conn.execute('SELECT * FROM COLEGIOS').fetchall()
+    conn.close()
+    return render_template('index.html', schools=schools)
